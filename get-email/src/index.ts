788724fb7c545f939c4515ff5e4977a85e66ffb5
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-27 14:30:23 +0900

    Implement deleteEmail

--- get-email/src/index.ts
+++ get-email/src/index.ts
@@ -32,27 +32,38 @@ admin.initializeApp({
 
 const db = admin.database();
 
-function registerEmail(uid: string, email: string){
+// ユーザーのメールアドレスを登録する関数
+function registerEmail(uid: string, email: string): void{
   const ref = db.ref("emails");
   const usersRef = ref.child(uid);
   usersRef.set({
-    "email": email
+    email: email
+  }, function(error) {
+    if (error) {
+      console.error("メールアドレスの登録に失敗しました", error);
+    } else {
+      console.log("メールアドレスをデータベースに登録しました");
+    }
   });
+}
 
-  ref.on("value",
-  function(snapshot) {
-    console.log("受け取ったメールアドレスをデータベースにアップロードしました");
-    console.log(snapshot.val());
-  }, 
-  function(errorObject) {
-      console.log("failed: " + errorObject);
+// ユーザーのメールアドレスを削除する関数
+function deleteEmail(uid: string): void{
+  const ref = db.ref("emails");
+  const usersRef = ref.child(uid);
+  usersRef.remove(function(error) {
+    if (error) {
+      console.error("メールアドレスの削除に失敗しました", error);
+    } else {
+      console.log("メールアドレスをデータベースから削除しました");
+    }
   });
 }
 
 
 // サーバーの処理
 app.get('/', (req: Request, res: Response) => {
-  res.send("Hello TypeScript")
+  res.send("Hello MySchedule Server")
 })
 
 // メールアドレス登録用のエンドポイントの設定
@@ -70,6 +81,22 @@ app.post("/register-email", async (req, res) => {
     }catch(e){
       console.log(e);
     }
+});
+
+// メールアドレス削除用のエンドポイントの設定
+app.post("/delete-email", async (req, res) => {
+  // corsの許可を設定する
+  res.header("Access-Control-Allow-Origin", "http://localhost:8090");
+  res.header("Access-Control-Allow-Methods", "POST");
+  // responseを処理する
+  console.log("/delete-email\nデータを受け取りました", req.body);
+  res.send("gotten\n"+JSON.stringify(req.body));
+  try{
+    const uid: string = req.body.uid;
+    deleteEmail(uid);
+  }catch(e){
+    console.log(e);
+  }
 });
 
 
