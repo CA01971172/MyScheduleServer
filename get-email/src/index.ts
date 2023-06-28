75b11dc549954cbdcbeca66abdeed6bc3358b132
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-28 10:04:48 +0900

    fix the port for release

--- get-email/src/index.ts
+++ get-email/src/index.ts
@@ -5,11 +5,12 @@ import bodyParser from 'body-parser';
 // firebaseモジュールのインポート
 import admin from "firebase-admin";
 
+// MyScheduleのアドレス
+const frontEndAddress: string = "http://192.168.5.55:8090";
 
 // expressの初期化
 const app: Express = express()
-// const port = process.env.PORT || 80
-const port = 8085;
+const port = process.env.PORT || 80
 
 // CORSミドルウェアの追加
 app.use(cors());
@@ -18,10 +19,10 @@ app.use(bodyParser.json());
 
 // firebaseの初期化
 // 1. サービスアカウント鍵を生成しserviceAccountKey.jsonでリネームしてfirebaseフォルダ直下に配置
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./firebase/serviceAccountKey.json");
 
 // firebaseの情報が入ったjsonファイルを取得する
-const firebaseUrl = require("./firebaseUrl.json");
+const firebaseUrl = require("./firebase/firebaseUrl.json");
 const databaseURL: string = firebaseUrl.firebaseUrl;
 
 admin.initializeApp({
@@ -63,13 +64,16 @@ function deleteEmail(uid: string): void{
 
 // サーバーの処理
 app.get('/', (req: Request, res: Response) => {
+  // corsの許可を設定する
+  res.header("Access-Control-Allow-Origin", frontEndAddress);
+  res.header("Access-Control-Allow-Methods", "GET");
   res.send("Hello MySchedule Server")
 })
 
 // メールアドレス登録用のエンドポイントの設定
 app.post("/register-email", async (req, res) => {
     // corsの許可を設定する
-    res.header("Access-Control-Allow-Origin", "http://localhost:8090");
+    res.header("Access-Control-Allow-Origin", frontEndAddress);
     res.header("Access-Control-Allow-Methods", "POST");
     // responseを処理する
     console.log("/register-email\nデータを受け取りました", req.body);
@@ -86,7 +90,7 @@ app.post("/register-email", async (req, res) => {
 // メールアドレス削除用のエンドポイントの設定
 app.post("/delete-email", async (req, res) => {
   // corsの許可を設定する
-  res.header("Access-Control-Allow-Origin", "http://localhost:8090");
+  res.header("Access-Control-Allow-Origin", frontEndAddress);
   res.header("Access-Control-Allow-Methods", "POST");
   // responseを処理する
   console.log("/delete-email\nデータを受け取りました", req.body);
